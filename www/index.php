5ff65999b8e3b1f2443e6c93dfec1be5c488f31e
DarkIguana <samvietreg@gmail.com>
2018-08-06 21:29:54 +0700

    .pasuletaem

--- www/index.php
+++ www/index.php
@@ -4,17 +4,17 @@ include_once '../config/db.php';  // инициализация БД
 include_once '../library/mainFunctions.php';
 include_once '../models/MenuModel.php'; // функции формрования меню
 
-// определяем с какой страной работаем
+// определяем (с какой страной работаем) какой крнтроллер-файл использовать 
 
 $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Excursions';
 
-// определяем какой раздел сайта
+// определяем какой (раздел сайта) Action выполнять 
 $actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
 
 // определяем какой id 
 $id = isset($_GET['id']) ? $_GET['id'] : null;
 
-// определяем (какой id) страну 
+// определяем  страну 
 $country = isset($_GET['country']) ? $_GET['country'] : 'thailand';
 
       
